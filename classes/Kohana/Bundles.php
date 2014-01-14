fab113e3c9c10edcda316f50338d4a091b799b46
Dmitry Momot <dmitry@dimkof.com>
2014-01-14 14:09:10 +0200

    added prefix to bundles name

--- classes/Kohana/Bundles.php
+++ classes/Kohana/Bundles.php
@@ -10,6 +10,12 @@ class Kohana_Bundles {
 	{
 		if ( ! empty($bundles) AND count($bundles) > 0)
 		{
+			foreach ($bundles as $bname => $bpath)
+			{
+				$bundles['bundle-'.$bname] = $bpath;
+				unset($bundles[$bname]);
+			}
+
 			Kohana::modules(Arr::merge($bundles, Kohana::modules()));
 		}
 	}
